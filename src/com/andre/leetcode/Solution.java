9dcc4d55756ba62bcdb6daecc7b365179a1b8774
yefxyz <yefxyz@outlook.com>
2016-12-28 00:18:12 +0800

    Palindrome Integer.

--- src/com/andre/leetcode/Solution.java
+++ src/com/andre/leetcode/Solution.java
@@ -394,4 +394,25 @@ public class Solution {
 		return sign * result;
 	}
 
+	/**
+	 * 判断是否为回文数。
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public static boolean isPalindrome(int x) {
+		if (x < 0 || x % 10 == 0) {
+			return false;
+		}
+		if (x < 10) {
+			return true;
+		}
+		int rev = 0;
+		while (x > rev) {
+			rev = rev * 10 + x % 10;
+			x = x / 10;
+		}
+		return x == rev || x == rev / 10;
+	}
+
 }
